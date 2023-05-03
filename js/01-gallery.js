07b8e6c46ed75ded04ef3e836a5e98d35e61961e
MakarenkoMargo <mv.frankova@sstc.ua>
2023-05-04 00:14:17 +0300

    done gallery task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `;
+    })
+    .join("");
+}
+console.log(createGalleryItemsMarkup(galleryItems));
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  oridinalGalleryElLink(e.target.dataset.source);
+}
+
+function oridinalGalleryElLink(src) {
+  basicLightbox
+    .create(
+      `
+		<img width="1400" height="900" src="${src}">
+	`
+    )
+    .show();
+}
+window.addEventListener("keydown", onEscKeyPress);
+function onEscKeyPress(e) {
+  const ESC_KEY_CODE = "Escape";
+  if (e.code === ESC_KEY_CODE) {
+    closeModal();
+  }
+}
+function closeModal() {
+  const instance = document.querySelector(".basicLightbox");
+  instance.classList.remove("basicLightbox--visible");
+  window.removeEventListener("keydown", onEscKeyPress);
+}
