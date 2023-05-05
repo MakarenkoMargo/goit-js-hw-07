2d4cd81aa95314eaa18ab32f36546bbbf7a620e3
MakarenkoMargo <mv.frankova@sstc.ua>
2023-05-05 22:49:50 +0300

    done tasl 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+           <a class="gallery__link" href="${original}">
+              <img class="gallery__image" src="${preview}" alt="${description}" />
+           </a>
+        </li>`;
+    })
+    .join("");
+}
+console.log(createGalleryItemsMarkup(galleryItems));
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
